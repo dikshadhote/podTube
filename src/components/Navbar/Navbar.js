c6b97e7e7f08064a65e9a2a4bcf2f149e79eb9a7
Diksha Dhote <dikshadhote7042@gmail.com>
2022-05-09 00:03:04 +0530

    feat: toggle sidebar Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiFillYoutube } from "react-icons/ai";
 import { FiSearch } from "react-icons/fi";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useSide } from "../../context/sidebar-context";
 export default function Navbar() {
+  const { toggleSidebar } = useSide();
   return (
     <div>
       <div className="nav-bar black-light-shade-bg">
         <div className="nav-logo-section ">
+          <GiHamburgerMenu
+            className="white-text-color ml-2 fs-2 mr-5 cursor-pointer"
+            onClick={() => toggleSidebar()}
+          />
           <div className="white-background size-yt-triangle pos-relative">
             <AiFillYoutube className="mr-1 red-yt-color fs-4 pos-abs-logo" />
           </div>
