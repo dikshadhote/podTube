ab7761328093a7e2d069a030b046705d70751fd5
Diksha Dhote <dikshadhote7042@gmail.com>
2022-05-08 14:40:02 +0530

    feat: search bar in Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,30 @@
 import React from "react";
-import { FaPodcast } from "react-icons/fa";
-
+import { AiFillYoutube } from "react-icons/ai";
+import { FiSearch } from "react-icons/fi";
 export default function Navbar() {
   return (
     <div>
-      <div className="nav-bar nav-yellow-shadow">
-        <div className="nav-logo-section">
-          <FaPodcast className="mb-1 mr-1" />
-          <h3 className="mr-4 aqua-color">podTube!</h3>
+      <div className="nav-bar black-light-shade-bg">
+        <div className="nav-logo-section ">
+          <div className="white-background size-yt-triangle pos-relative">
+            <AiFillYoutube className="mr-1 red-yt-color fs-4 pos-abs-logo" />
+          </div>
+          <p className="mr-4 ml-2 white-text-color brand-name">PodTube</p>
+        </div>
+        <div>
+          <div className="border-none search-box d-flex border-radius">
+            <input type="text" placeholder="Search" id="search" />
+            <button className="d-flex align-items-center border-none">
+              <FiSearch size={20} />
+            </button>
+          </div>
         </div>
         <div className="nav-items">
-          <a className="black-text-color">
-            <span
-              className="material-icons ml-1 aqua-color"
-              title="profile details"
-            >
+          <a className="blue-yt-text d-flex align-items-center cursor-pointer ">
+            <span className="material-icons ml-1 blue-yt-text" title="Sign in">
               account_circle
             </span>
+            <p className="ml-1">Sign in</p>
           </a>
         </div>
       </div>
