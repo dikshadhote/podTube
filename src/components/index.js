598ac1fb2d78f896e8908ff880cc96a2103e7859
Diksha Dhote <dikshadhote7042@gmail.com>
2022-05-08 10:14:14 +0530

    fix: change import according to default export

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,11 @@
-import { Home } from "./Home/Home";
-import { History } from "./History/History";
-import { LikeVideos } from "./LikedVideos/LikedVideos";
-import { Navbar } from "./Navbar/Navbar";
-import { Sidebar } from "./Sidebar/Sidebar";
-import { WatchLater } from "./WatchLater";
-import { Login } from "./Login/Login";
-import { SignUp } from "./SignUp/SignUp";
+import Home from "./Home/Home";
+import History from "./History/History";
+import LikeVideos from "./LikedVideos/LikedVideos";
+import Navbar from "./Navbar/Navbar";
+import Sidebar from "./Sidebar/Sidebar";
+import WatchLater from "./WatchLater/WatchLater";
+import Login from "./Login/Login";
+import SignUp from "./SignUp/SignUp";
 export {
   Home,
   History,
