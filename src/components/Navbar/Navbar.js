36b85cb5afb879be8f259ae9702f43dc19ba623c
Diksha Dhote <dikshadhote7042@gmail.com>
2022-05-08 10:14:51 +0530

    feat: display navbar in home

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { FaPodcast } from "react-icons/fa";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div className="nav-bar nav-yellow-shadow">
+        <div className="nav-logo-section">
+          <FaPodcast className="mb-1 mr-1" />
+          <h3 className="mr-4 aqua-color">podTube!</h3>
+        </div>
+        <div className="nav-items">
+          <a className="black-text-color">
+            <span
+              className="material-icons ml-1 aqua-color"
+              title="profile details"
+            >
+              account_circle
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
