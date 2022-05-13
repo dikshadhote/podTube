e5f2facabc95cce422a80c195f752278dd5aff52
Diksha Dhote <dikshadhote7042@gmail.com>
2022-05-13 20:53:25 +0530

    feat: import thumbnails in db

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,46 @@
-import powerOfFitness from "./powerOfFitness.jpg";
 import avatar1 from "./avatar1.png";
 import avatar2 from "./avatar2.png";
 import avatar3 from "./avatar3.png";
-export { powerOfFitness, avatar1, avatar2, avatar3 };
+import atomicHabits from "./atomicHabits.jpg";
+import beginnerStockMarket from "./beginnerStockMarket.jpg";
+import blockchain from "./blockchain.jpg";
+import buildSideProjects from "./buildSideProjects.jpg";
+import dietMentalPhysical from "./dietMentalPhysical.jpg";
+import efficentDevImage from "./efficentDevImage.jpg";
+import ExerciseParadox from "./ExerciseParadox.jpg";
+import fifteenBooks22 from "./fifteenBooks22.jpg";
+import finanacialBasics from "./finanacialBasics.jpg";
+import financebeginner from "./financebeginner.jpg";
+import futureElon from "./futureElon.jpg";
+import getRichNaval from "./getRichNaval.jpg";
+import mentalHealthAnna from "./mentalHealthAnna.jpg";
+import mustReadBooksMS from "./mustReadBooksMS.jpg";
+import personalFinance21 from "./personalFinance21.jpg";
+import powerOfFitness from "./powerOfFitness.jpg";
+import steveJobs from "./steveJobs.jpg";
+import whatMentalHealth from "./whatMentalHealth.jpg";
+import youngestBillionare from "./youngestBillionare.jpg";
+export {
+  avatar1,
+  avatar2,
+  avatar3,
+  atomicHabits,
+  beginnerStockMarket,
+  blockchain,
+  buildSideProjects,
+  dietMentalPhysical,
+  efficentDevImage,
+  ExerciseParadox,
+  fifteenBooks22,
+  finanacialBasics,
+  financebeginner,
+  futureElon,
+  getRichNaval,
+  mentalHealthAnna,
+  mustReadBooksMS,
+  personalFinance21,
+  powerOfFitness,
+  steveJobs,
+  whatMentalHealth,
+  youngestBillionare,
+};
--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -3,6 +3,30 @@
  * You can add videos of your wish with different attributes
  * */
 import { v4 as uuid } from "uuid";
+import {
+  avatar1,
+  avatar2,
+  avatar3,
+  atomicHabits,
+  beginnerStockMarket,
+  blockchain,
+  buildSideProjects,
+  dietMentalPhysical,
+  efficentDevImage,
+  ExerciseParadox,
+  fifteenBooks22,
+  finanacialBasics,
+  financebeginner,
+  futureElon,
+  getRichNaval,
+  mentalHealthAnna,
+  mustReadBooksMS,
+  personalFinance21,
+  powerOfFitness,
+  steveJobs,
+  whatMentalHealth,
+  youngestBillionare,
+} from "../../assets";
 export const videos = [
   {
     _id: uuid(),
@@ -12,6 +36,10 @@ export const videos = [
     description:
       "Why exercise is an important part of a healthy lifestyle. Vincent Lam has had a passion for fitness for as long as he can remember. His goal is to help people restore, maintain, and promote quality of life and optimal physical function. He is devoted to helping others achieve their fitness goals. He graduated from Penn State with a B.S. in kinesiology and is also a certified personal trainer. This talk was given at a TEDx event using the TED conference format but independently organized by a local community. ",
     creator: "TEDx",
+    avatar: avatar2,
+    thumbUrl: powerOfFitness,
+    duration: "15:44",
+    views: "33.4k",
   },
 
   {
@@ -22,6 +50,10 @@ export const videos = [
     description:
       "hin Kamath is back again. Part 1 was all about the story of Zerodha. The real bootstrapped success story. ",
     creator: "BeerBiceps",
+    avatar: avatar1,
+    thumbUrl: finanacialBasics,
+    duration: "15:44",
+    views: "33.4k",
   },
   {
     _id: uuid(),
@@ -30,6 +62,10 @@ export const videos = [
     categoryName: "Book",
     creator: "Ali Abdaal ",
     description: "In this video Ali Abdaal suggests her book recommendation",
+    avatar: avatar3,
+    thumbUrl: fifteenBooks22,
+    duration: "15:44",
+    views: "33.4k",
   },
   {
     _id: uuid(),
@@ -39,15 +75,22 @@ export const videos = [
     creator: "TEDx",
     description:
       "How can you manipulate your brain to achieve greater levels of happiness? Can you be so inspired that your daily exercises bring you joy and increase your self-esteem? By practicing the four key strategies given in this TEDx talk, we can all achieve greater levels of energy, mood and overall health along with positive long-term changes in our physiques.",
+    avatar: avatar2,
+    thumbUrl: ExerciseParadox,
+    duration: "15:44",
+    views: "33.4k",
   },
   {
     _id: uuid(),
     title: "Get rich without getting lucky",
     categoryName: "Finance",
     videoUrl: " https://www.youtube.com/watch?v=1-TZqOsVCNM",
-    description:
-      "hin Kamath is back again. Part 1 was all about the story of Zerodha. The real bootstrapped success story. ",
-    creator: "BeerBiceps",
+    description: "Naval story ",
+    creator: "Naval",
+    avatar: avatar2,
+    thumbUrl: getRichNaval,
+    duration: "15:44",
+    views: "33.4k",
   },
   {
     _id: uuid(),
@@ -57,6 +100,10 @@ export const videos = [
     creator: "The School of Life",
     description:
       "Nowadays, we know more and more about what it means to be mentally unwell - but what exactly constitutes mental 'health’? In this film, we’ll explore what characterises an optimally functioning mind. ",
+    avatar: avatar2,
+    thumbUrl: whatMentalHealth,
+    duration: "15:44",
+    views: "33.4k",
   },
   {
     _id: uuid(),
@@ -66,6 +113,10 @@ export const videos = [
     creator: "Raj Shamani",
     description:
       "Nikhil Kamath, 35, co-founded Zerodha, India's largest trading platform by volume, with his older brother Nithin in 2010 in the southern Indian city of Bangalore. In this video, he discusses how he moved out of his house and went from working at a Call Centre to Building up One of the Largest Businesses and becoming India's Youngest Billionaire! ",
+    avatar: avatar1,
+    thumbUrl: youngestBillionare,
+    duration: "15:44",
+    views: "33.4k",
   },
   {
     _id: uuid(),
@@ -74,6 +125,10 @@ export const videos = [
     categoryName: "Book",
     creator: "Readers book club ",
     description: "This video contain summary of Atomic Habits",
+    avatar: avatar3,
+    thumbUrl: atomicHabits,
+    duration: "15:44",
+    views: "33.4k",
   },
   {
     _id: uuid(),
@@ -83,6 +138,10 @@ export const videos = [
     creator: "Anna Freud NCCF",
     description:
       "We All Have Mental Health is an animation designed to give young people aged 11-14 a common language and understanding of what we mean by mental health and how we can look after it. It has been created for young people in Key stage 3 and can be used with accompanying teaching resources.. ",
+    avatar: avatar2,
+    thumbUrl: mentalHealthAnna,
+    duration: "15:44",
+    views: "33.4k",
   },
   {
     _id: uuid(),
@@ -92,6 +151,10 @@ export const videos = [
     creator: "Tanay Pratap",
     description:
       "In episode 2, Rachit Gulati from Microsoft joins Tanay to talk about how he got started with hobby projects and how they helped him in cracking interviews. Rachit goes on to explain how once can go about doing hobby projects and benefit from them. ",
+    avatar: avatar2,
+    thumbUrl: buildSideProjects,
+    duration: "15:44",
+    views: "33.4k",
   },
   {
     _id: uuid(),
@@ -101,6 +164,10 @@ export const videos = [
     creator: "Pranjal Kamra",
     description:
       "Stock Market for Beginners is the introductory video for everyone who is interested in investing in the share market. ",
+    avatar: avatar1,
+    thumbUrl: beginnerStockMarket,
+    duration: "15:44",
+    views: "33.4k",
   },
   {
     _id: uuid(),
@@ -109,16 +176,12 @@ export const videos = [
     categoryName: "Technology",
     creator: "BeerBiceps ",
     description: "How blockchain works by Indian Crypto millionaire",
+    avatar: avatar1,
+    thumbUrl: blockchain,
+    duration: "15:44",
+    views: "33.4k",
   },
-  {
-    _id: uuid(),
-    title: "We all have mental health",
-    videoUrl: "https://youtu.be/DxIDKZHW3-E",
-    categoryName: "Health",
-    creator: "Anna Freud NCCF",
-    description:
-      "We All Have Mental Health is an animation designed to give young people aged 11-14 a common language and understanding of what we mean by mental health and how we can look after it. It has been created for young people in Key stage 3 and can be used with accompanying teaching resources.. ",
-  },
+
   {
     _id: uuid(),
     title: "Personal Financial Stratergies 2021",
@@ -127,6 +190,10 @@ export const videos = [
     creator: "Ankoor warikoo ",
     description:
       "Budgeting your #money is the key to having enough,” said Elizabeth Warren.Since we have entered a new financial year, in this video I will share my #PersonalFinance resolutions that I have planned for the next twelve months. And yes, I’ve been doing it since my college days when I used to spend Rs. 7,500 per month. So these budgeting, allocations and places where I will spend my money without thinking – apply even to college kid ",
+    avatar: avatar1,
+    thumbUrl: personalFinance21,
+    duration: "15:44",
+    views: "33.4k",
   },
   {
     _id: uuid(),
@@ -135,6 +202,10 @@ export const videos = [
     categoryName: "Book",
     creator: "Mostlysane ",
     description: "In this video mostlysane suggests her book recommendation",
+    avatar: avatar3,
+    thumbUrl: mustReadBooksMS,
+    duration: "15:44",
+    views: "33.4k",
   },
   {
     _id: uuid(),
@@ -144,6 +215,10 @@ export const videos = [
     creator: "TED ",
     description:
       "Even if a 10x developer may be a myth, we all know of some developer that just shines and is able to do more in less time, and seems to do it without effort! Even if may seem that you need some kind of special natural talent, it is not.In this session by Daniel Lebrero you can learn what you need to learn to become a more efficient developer.",
+    avatar: avatar1,
+    thumbUrl: efficentDevImage,
+    duration: "15:44",
+    views: "33.4k",
   },
   {
     _id: uuid(),
@@ -153,7 +228,12 @@ export const videos = [
     creator: "Dr Caroline Leaf",
     description:
       "In this podcast I talk to Harvard-trained nutritional psychiatrist Dr. Georgia Ede about how certain diets can affect our mental wellbeing, what foods help the brain function best, and what foods to avoid if we want to heal and improve our mental and physical health. ",
+    avatar: avatar2,
+    thumbUrl: dietMentalPhysical,
+    duration: "15:44",
+    views: "33.4k",
   },
+
   {
     _id: uuid(),
     title: "Financial planning for beginners",
@@ -161,6 +241,10 @@ export const videos = [
     categoryName: "Finance",
     creator: "CA Rachana Ranade ",
     description: "A CA explains basic finance for beginners ",
+    avatar: avatar1,
+    thumbUrl: financebeginner,
+    duration: "15:44",
+    views: "33.4k",
   },
   {
     _id: uuid(),
@@ -170,6 +254,10 @@ export const videos = [
     creator: "TED ",
     description:
       "Elon Musk discusses his new project digging tunnels under LA, the latest from Tesla and SpaceX and his motivation for building a future on Mars in conversation with TED's Head Curator, Chris Anderson ",
+    avatar: avatar1,
+    thumbUrl: futureElon,
+    duration: "15:44",
+    views: "33.4k",
   },
   {
     _id: uuid(),
@@ -178,5 +266,9 @@ export const videos = [
     categoryName: "Book",
     creator: "Alux.com ",
     description: "Steve jobs book recommendation",
+    avatar: avatar3,
+    thumbUrl: steveJobs,
+    duration: "15:44",
+    views: "33.4k",
   },
 ];
