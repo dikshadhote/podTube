24193140676e06faead0717fa69e8ddde990c303
Diksha Dhote <dikshadhote7042@gmail.com>
2022-05-13 23:34:29 +0530

    feat:fetch video data, reducer,slice

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-
 import authReducer from "../reducers/auth/authSlice";
+import videosReducer from "../reducers/video-listing/videosSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    video: videosReducer,
   },
 });
