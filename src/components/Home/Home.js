c409558137e7b7bc1452057e1a1e3f0c76644248
Diksha Dhote <dikshadhote7042@gmail.com>
2022-05-14 22:47:35 +0530

    fix:optional panel opening for every card

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getVideos } from "../../redux/reducers/video-listing/videosSlice";
 import { getCategories } from "../../redux/reducers/video-listing/categorySlice";
 import { BsThreeDotsVertical } from "react-icons/bs";
-
+import { MdVideoLibrary, MdThumbUp } from "react-icons/md";
 export default function Home() {
   const videos = useSelector((state) => state.video.videos);
   const categories = useSelector((state) => state.categories.categories);
-  console.log(categories);
+  const [showPanel, setShowPanel] = useState(false);
+  const [saveId, showSaveid] = useState(false);
+  const [filteredList, setFilteredList] = useState([]);
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -15,12 +17,27 @@ export default function Home() {
     dispatch(getCategories());
   }, [dispatch]);
 
+  const handleShowPanel = (inputId) => {
+    showSaveid(inputId);
+    const videoPresent = videos.some((video) => video._id === inputId);
+
+    if (videoPresent) {
+      setShowPanel(true);
+    }
+  };
+
   return (
     <div className="d-flex flex-column">
       <div className="mt-3 mb-1 gray-border-chip-container pb-2 pt-2">
+        <span className="card-category-txt  white-text-color cursor-pointer">
+          All
+        </span>
         {categories.map(({ categoryName }, idx) => {
           return (
-            <span className="card-category-txt  white-text-color" key={idx}>
+            <span
+              className="card-category-txt  white-text-color cursor-pointer"
+              key={idx}
+            >
               {categoryName}
             </span>
           );
@@ -28,11 +45,11 @@ export default function Home() {
       </div>
       <div className="d-flex flex-wrap">
         {videos.map(
-          ({ title, thumbUrl, creator, views, duration, avatar }, idx) => {
+          ({ title, thumbUrl, creator, views, duration, avatar, _id }) => {
             return (
               <div
                 className="card flex-column card-vert black-dark-bg border-none cursor-pointer"
-                key={idx}
+                key={_id}
               >
                 <div className="">
                   <img
@@ -61,6 +78,31 @@ export default function Home() {
                           {views + " "}views
                         </p>
                       </div>
+                    </div>
+                    <div className="pos-relative">
+                      <BsThreeDotsVertical
+                        className="white-text-color fs-2 "
+                        onClick={() => handleShowPanel(_id)}
+                      />
+                      {showPanel && saveId == _id && (
+                        <div
+                          className="popup-threedot d-flex flex-column "
+                          onClick={() => setShowPanel(!showPanel)}
+                        >
+                          <div className="d-flex mb-1">
+                            <MdThumbUp className="fs-2 white-text-color mr-2" />
+                            <p className="white-text-color font-weight-bold">
+                              Like
+                            </p>
+                          </div>
+                          <div className="d-flex mb-1">
+                            <MdVideoLibrary className="fs-2 white-text-color mr-2" />
+                            <p className="white-text-color font-weight-bold">
+                              Save to watch later
+                            </p>
+                          </div>
+                        </div>
+                      )}
                     </div>
                   </div>
                 </div>
