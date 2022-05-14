bfca454ce9ec2ac63678fadbff0496889fadc44a
Diksha Dhote <dikshadhote7042@gmail.com>
2022-05-14 14:35:52 +0530

    feat: category slice and api call

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../reducers/auth/authSlice";
 import videosReducer from "../reducers/video-listing/videosSlice";
+import categoryReducer from "../reducers/video-listing/categorySlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     video: videosReducer,
+    categories: categoryReducer,
   },
 });
