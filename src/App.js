21e8d4676cb8186fb66855452e919ffd1edee866
Diksha Dhote <dikshadhote7042@gmail.com>
2022-05-18 23:36:40 +0530

    feat: react-toastify install

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Router from "./router/Router";
-
+import { ToastContainer } from "react-toastify";
 function App() {
   return (
     <div className="App">
       <Router />
+      <ToastContainer />
     </div>
   );
 }
