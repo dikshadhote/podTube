dd5acfba51849a31e76b1b1f475a70e8f1bb5442
Diksha Dhote <dikshadhote7042@gmail.com>
2022-05-19 15:20:48 +0530

    feat:like reducer

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../reducers/auth/authSlice";
 import videosReducer from "../reducers/video-listing/videosSlice";
 import categoryReducer from "../reducers/video-listing/categorySlice";
+import likeReducer from "../reducers/like/likeSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     video: videosReducer,
     categories: categoryReducer,
+    like: likeReducer,
   },
 });
