c6b29d0363f8233d3d25cdfac2960a597650ca10
Diksha Dhote <dikshadhote7042@gmail.com>
2022-05-20 11:18:29 +0530

    feat: watch later reducer

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -3,11 +3,13 @@ import authReducer from "../reducers/auth/authSlice";
 import videosReducer from "../reducers/video-listing/videosSlice";
 import categoryReducer from "../reducers/video-listing/categorySlice";
 import likeReducer from "../reducers/like/likeSlice";
+import watchlaterReducer from "../reducers/watch-later/watchlaterSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     video: videosReducer,
     categories: categoryReducer,
     like: likeReducer,
+    watchlater: watchlaterReducer,
   },
 });
