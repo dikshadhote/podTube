d5f51f6ff2a0cc3e3b1b340c10475b65d2aadd20
Diksha Dhote <dikshadhote7042@gmail.com>
2022-05-26 16:00:52 +0530

    feat: Display videos in playlist

--- src/components/playlist/Playlist.jsx
+++ src/components/playlist/Playlist.jsx
@@ -1,6 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getAllPlaylists } from "../../redux/reducers/playlist/playlistSlice";
+import { useSelector, useDispatch } from "react-redux";
 
+import VideoCard from "../VideoCard/VideoCard";
 export default function Playlist() {
-  console.log("In playist");
-  return <div>Playlist</div>;
+  const playlistDispatch = useDispatch();
+
+  useEffect(() => {
+    playlistDispatch(getAllPlaylists());
+  }, [playlistDispatch]);
+
+  const playlistData = useSelector((state) => state.playlists.playlists);
+  console.log(playlistData);
+  return (
+    <div>
+      {playlistData.length > 0 ? (
+        <div>
+          {playlistData.map((playlist) => {
+            return (
+              <div>
+                <div className="d-flex justify-content center white-text-color">
+                  <h4 className="mt-2 ml-2 white-text-color">
+                    {playlist.title}
+                  </h4>
+                </div>
+                <div className="d-flex flex-wrap">
+                  {playlist.videos.map((video) => {
+                    return <VideoCard video={video} key={video._id} />;
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="d-flex justify-content center white-text-color">
+          <h4 className="mt-2 ml-2">
+            Please create playlist from home page...
+          </h4>
+        </div>
+      )}
+    </div>
+  );
 }
